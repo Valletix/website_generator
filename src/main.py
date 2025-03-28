f76a4cfee6def54a407d931bd0e6308e524a28a1
Valletix <Valltix23@gmail.com>
2025-03-28 11:54:20 +0100

    Changed some git settings

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 import os
 import shutil
 from markdown_blocks import markdown_to_html_node
+import sys
 
 def main():
+    if len(sys.argv) == 1:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1]
     shutil.rmtree("public/", True)
     print("Deleted the public directory")
     os.mkdir("public")
     print("created a new public directory")
     del_and_copy_all("static/", "public/")
-    generate_pages_recursive("content/", "template.html", "public/")
+    generate_pages_recursive("content/", "template.html", "docs/", basepath)
 
 
 def del_and_copy_all(source_dir, dest_dir):
@@ -30,7 +35,7 @@ def extract_title(markdown):
             return line.lstrip("#").strip(" ")
     raise Exception("No h1 header in file")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     md_file = open(from_path).read()
     template_file = open(template_path).read()
@@ -38,13 +43,14 @@ def generate_page(from_path, template_path, dest_path):
     html_string = html_node.to_html()
     title = extract_title(md_file)
     new_template = template_file.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    replaced_template = new_template.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     if not os.path.exists(dest_path):
         os.makedirs(dest_path)
     index_file = open(dest_path + "index.html", "x+")
-    index_file.write(new_template)
+    index_file.write(replaced_template)
     index_file.close()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if not os.path.exists(dest_dir_path):
         os.mkdir(dest_dir_path)
         print(f"created new {dest_dir_path} folder")
@@ -58,11 +64,12 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             html_string = html_node.to_html()
             title = extract_title(md_file)
             new_template = template_file.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+            replaced_template = new_template.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
             new_file = open(dest_dir_path + filename + ".html", "x+")
-            new_file.write(new_template)
+            new_file.write(replaced_template)
             new_file.close()
         else:
-            generate_pages_recursive(f"{dir_path_content}{entry}/",template_path, f"{dest_dir_path}{entry}/")
+            generate_pages_recursive(f"{dir_path_content}{entry}/",template_path, f"{dest_dir_path}{entry}/", basepath)
 
 
 
